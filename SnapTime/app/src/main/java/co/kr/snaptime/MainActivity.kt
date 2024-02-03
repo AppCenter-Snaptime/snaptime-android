0b513f6959d51be582733875e64970500febcf1a
shin hye mi <hfzgky@gmail.com>
2024-02-03 20:35:36 +0900

    Test:Hyemi

--- SnapTime/app/src/main/java/co/kr/snaptime/MainActivity.kt
+++ SnapTime/app/src/main/java/co/kr/snaptime/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Hyemi")
                 }
             }
         }
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     SnapTimeTheme {
-        Greeting("Android")
+        Greeting("Hyemi")
     }
 }
