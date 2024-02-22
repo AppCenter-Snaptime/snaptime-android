fc61f606eb4397fa1b588bd2da4126c7f3e6b98e
choiMY <thskan123439@gmail.com>
2024-02-23 05:16:35 +0900

    Design: album cardview 추가

---
+++ SnapTime/app/src/main/java/co/kr/snaptime/component/AlbumCardView.kt
@@ -0,0 +1,61 @@
+package co.kr.snaptime.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import co.kr.snaptime.R
+
+@Composable
+fun AlbumCardView(
+    paintId: Int,
+    albumTitle: String
+) {
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        shape = RoundedCornerShape(4.dp),
+        colors = CardDefaults.cardColors(Color.White)
+    ) {
+        Image(
+            modifier = Modifier.clip(RectangleShape),
+            painter = painterResource(id = paintId),
+            contentDescription = "앨범"
+        )
+
+        Row(
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(8.dp, 4.dp, 0.dp, 4.dp),
+
+        ) {
+            Text(
+                text = albumTitle,
+                fontSize = 12.sp
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreAlbumCard() {
+    AlbumCardView(
+        R.drawable.ic_launcher_foreground,
+        "최근 항목"
+    )
+}
