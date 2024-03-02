1aa8fa91c7dc30d61243ccd41999656f5e8c93dc
choiMY <thskan123439@gmail.com>
2024-03-02 19:34:57 +0900

    Design: 폰트 추가

--- SnapTime/app/src/main/java/co/kr/snaptime/ui/theme/Type.kt
+++ SnapTime/app/src/main/java/co/kr/snaptime/ui/theme/Type.kt
@@ -2,10 +2,20 @@ package co.kr.snaptime.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import co.kr.snaptime.R
 
+// Apple SD Gothic Neo font family
+val AppleSDGothicNeo = FontFamily(
+    Font(R.font.applesdgothicneo_b, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.applesdgothicneo_eb, FontWeight.ExtraBold, FontStyle.Normal),
+    Font(R.font.applesdgothicneo_sb, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.applesdgothicneo_r, FontWeight.Normal, FontStyle.Normal)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
