9ed164da38a72ef7fb5b46a7596c1e40e64357df
choiMY <thskan123439@gmail.com>
2024-04-03 12:06:24 +0900

    Design: 프로필 편집 UI 완성

--- SnapTime/app/src/main/java/co/kr/snaptime/MainActivity.kt
+++ SnapTime/app/src/main/java/co/kr/snaptime/MainActivity.kt
@@ -23,25 +23,16 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     AppNavHost()
-                    //Greeting("Android")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SnapTimeTheme {
-        Greeting("Android")
+        AppNavHost()
     }
 }
