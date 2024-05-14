da7aa5b4f2df014ed39bdfd45008c4d3e8e6c65b
choiMY <thskan123439@gmail.com>
2024-05-14 15:09:53 +0900

    Design: HomeScreen 틀 제작

--- SnapTime/app/src/main/java/co/kr/snaptime/screen/HomeScreen.kt
+++ SnapTime/app/src/main/java/co/kr/snaptime/screen/HomeScreen.kt
@@ -1,34 +1,82 @@
 package co.kr.snaptime.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Menu
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import co.kr.snaptime.component.Appbar
+import androidx.navigation.compose.rememberNavController
+import co.kr.snaptime.R
+import co.kr.snaptime.component.AlbumCardView
+import co.kr.snaptime.component.NoNavIconAppbar
+import co.kr.snaptime.component.STBottomAppBar
+import co.kr.snaptime.ui.theme.MainColor_blue
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     navController: NavController
 ) {
+    Scaffold(
+        topBar = {
+            NoNavIconAppbar(
+                title = "SnapTime",
+                menuIcon = null
+            )
+        },
+        bottomBar = {
+            STBottomAppBar()
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+                shape = CircleShape,
+                containerColor = MainColor_blue
+            ) {
+                // 내부에 들어가는 Icon
+            }
+        }
+    ) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier
+                .padding(it)
+                .padding(top = 8.dp, start = 8.dp, end = 8.dp, bottom = 4.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(5) {
+                AlbumCardView(
+                    paintId = R.drawable.ic_launcher_foreground,
+                    albumTitle = "최근 항목"
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreHomeScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.Gray),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Appbar(
-            title = "Home",
-            navIcon = Icons.Filled.Home,
-            onNavClick = { /*TODO*/ },
-            menuIcon = Icons.Filled.Menu
+            .background(color = Color.White)
+    ) {
+        HomeScreen(
+            navController = rememberNavController()
         )
     }
 }
